6e3674e9fc0889dc3086324e6447d43211f06a36
Nicola Musatti <nicola.musatti@gmail.com>
2022-04-26 09:19:56 +0200

    Add Files utility class

---
+++ src/test/java/it/twenfir/antlr/tools/FilesTest.java
@@ -0,0 +1,18 @@
+package it.twenfir.antlr.tools;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.junit.Test;
+
+public class FilesTest {
+	
+    @Test
+    public void readResourceTest() throws IOException {
+    	String s = Files.readFile("logback.xml", Arrays.asList("classpath:"));
+    	assertTrue(s.indexOf("twenfir") != -1);
+    }
+
+}
