4ba348d0b602e9dc437ccc5cd4eb5fb7e56f4630
Nicola Musatti <nicola.musatti@gmail.com>
2024-01-28 11:52:20 +0100

    Add isErrors() method

--- src/main/java/it/twenfir/antlr/api/ErrorListener.java
+++ src/main/java/it/twenfir/antlr/api/ErrorListener.java
@@ -4,15 +4,40 @@ import org.antlr.v4.runtime.ANTLRErrorListener;
 
 import it.twenfir.antlr.ast.AstNode;
 
+/**
+ * Error reporting interface for parsers that build ASTs
+ */
 public interface ErrorListener extends ANTLRErrorListener {
 
 	/**
 	 * Notify an AST building error.
 	 * 
-	 * @param node 
-	 * @param msg
+	 * @param node	the node that was being handled when the error occurred
+	 * @param msg	the error message
 	 */
 	public void astError(AstNode node, String msg);
+
+	/**
+	 * Notify an AST building error in the presence of an exception.
+	 * 
+	 * @param node	the node that was being handled when the error occurred
+	 * @param msg	the error message
+	 * @param e		the exception that notified the error in the first place
+	 */
 	public void astError(AstNode node, String msg, RuntimeException e);
+
+	/**
+	 * Notify an AST building warning.
+	 * 
+	 * @param node	the node that was being handled when the warning condition occurred
+	 * @param msg	the warning message
+	 */
 	public void astWarning(AstNode node, String msg);
+	
+	/**
+	 * Indicate whether errors have occured.
+	 * 
+	 * @return true if one or more errors have occurred, false otherwise
+	 */
+	public boolean isErrors();
 }
