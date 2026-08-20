1cf5514f7fd9b4e9c2d6bc99176b6eda201e0f4c
Nicola Musatti <nicola.musatti@gmail.com>
2026-08-20 18:14:30 +0200

    Correct last merge

--- src/main/java/it/twenfir/antlr/api/SourceFile.java
+++ src/main/java/it/twenfir/antlr/api/SourceFile.java
@@ -2,23 +2,37 @@ package it.twenfir.antlr.api;
 
 /**
  * A source file as read by Files.readFile()
- * @param path	the file's path
- * @param text	the file's contents
  */
 public class SourceFile {
 	
 	private String path;
 	private String text;
-	
+
+	/*
+	 * Constructor
+	 * 
+	 * @param path	the file's path
+	 * @param text	the file's contents
+	 */
 	public SourceFile(String path, String text) {
 		this.path = path;
 		this.text = text;
 	}
 
+	/**
+	 * Getter
+	 * 
+	 * @return the source file path
+	 */
 	public String getPath() {
 		return path;
 	}
 	
+	/**
+	 * Getter
+	 * 
+	 * @return the source file text
+	 */
 	public String getText() {
 		return text;
 	}
